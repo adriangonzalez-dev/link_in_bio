b1a77e325e6d42ff70b37de7455589986a8216f6
adriang1990 <adriang@siainteractive.com>
2024-08-18 15:51:58 -0300

    v1

---
+++ src/components/icons/index.ts
@@ -0,0 +1,9 @@
+import Facebook from "./Facebook.astro";
+import Instagram from "./Instagram.astro";
+import Twitter from "./Twitter.astro";
+
+export {
+    Facebook,
+    Instagram,
+    Twitter
+}
